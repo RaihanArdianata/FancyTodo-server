618a2102cf3a4131fc410e61470917e0c111435f
RaihanArdianata <RaihanArdianata@users.noreply.github.com>
2020-03-30 23:15:41 +0700

    done crud

--- models/todo.js
+++ models/todo.js
@@ -1,31 +1,42 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
 
-  const {Model} = sequelize.Sequelize
+  const { Model } = sequelize.Sequelize
 
-  class Todo extends Model{}
+  class Todo extends Model { }
 
   Todo.init(
     {
-      title: DataTypes.STRING,
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: `the title cant null`
+          },
+          notEmpty: {
+            msg: `the title cant empty`
+          }
+        }
+      },
       description: DataTypes.STRING,
       status: DataTypes.BOOLEAN,
-      due_date:{
+      due_date: {
         type: DataTypes.DATE,
-        validate:{
-          checkDueDate(){
-            if(this.due_date < new Date()){
-              throw new Error ('Error Date cant less than current date')
+        validate: {
+          checkDueDate() {
+            if (this.due_date < new Date()) {
+              throw new Error('Error Date cant less than current date')
             }
           }
         }
-      } 
+      }
     }, {
-      sequelize
-    }
+    sequelize
+  }
   )
 
-  Todo.associate = function(models) {
+  Todo.associate = function (models) {
     // associations can be defined here
   };
   return Todo;
